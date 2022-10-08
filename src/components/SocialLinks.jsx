79c532ab41cc499eabbeb58eb1561381e81e29ab
iammeskat <meskatcse@gmail.com>
2022-10-09 00:11:52 +0600

    add some icons

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -6,7 +6,11 @@ const SocialLinks = () => {
       <SocialLink url="https://github.com/iammeskat" icon="github.svg" />
       <SocialLink url="https://linkedin.com/in/meskat" icon="linkedin.svg" />
       <SocialLink url="https://twitter.com/m35k47" icon="twitter.svg" />
-      <SocialLink url="https://facebook.com/m35k47" icon="github.svg" />
+      <SocialLink url="https://facebook.com/m35k47" icon="facebook.svg" />
+      <SocialLink
+        url="https://www.instagram.com/meskatul.islam/"
+        icon="instagram.svg"
+      />
     </div>
   );
 };
--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -2,7 +2,7 @@ export  const skillsData = [
     { title: "laravel", icon: "laravel.svg" },
     { title: "react.js", icon: "react.svg" },
     { title: "node.js", icon: "nodejs.svg" },
-    { title: "express.js", icon: "expressjs.png" },
+    { title: "express.js", icon: "express-js.svg" },
     { title: "tailwind css", icon: "tailwindcss.svg" },
     { title: "bootstrap", icon: "bootstrap.svg" },
     { title: "php", icon: "php.svg" },
