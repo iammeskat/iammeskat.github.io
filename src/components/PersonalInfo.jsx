0dfa7817bb336c0135e665ff49fea796b33a8e09
iammeskat <meskatcse@gmail.com>
2022-10-11 00:50:50 +0600

    redesign personal info section

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -4,65 +4,43 @@ const PersonalInfo = () => {
   return (
     <div className="space-y-3 md:space-y-6">
       <h2 className="font-bold text-xl md:text-3xl uppercase">personal info</h2>
-      <table className="w-full">
-        <tbody>
-          <tr>
-            <td className="py-1.5 align-text-top">
-              <p className="font-mediumm text-lg">
-                <span className="font-thin text-gray-400">Name:</span> Meskatul
-                Islam
-              </p>
-            </td>
-            <td className="py-1.5 align-text-top">
-              <p className="font-mediumm text-lg">
-                <span className="font-thin text-gray-400">Freelance:</span>{" "}
-                Available
-              </p>
-            </td>
-          </tr>
-          <tr>
-            <td className="py-1.5 align-text-top">
-              <p className="font-mediumm text-lg">
-                <span className="font-thin text-gray-400">Age:</span> 24
-              </p>
-            </td>
-            <td className="py-1.5 align-text-top">
-              <p className="font-mediumm text-lg">
-                <span className="font-thin text-gray-400">Skills:</span> Backend
-                & Web Frontend
-              </p>
-            </td>
-          </tr>
-          <tr>
-            <td className="py-1.5 align-text-top">
-              <p className="font-mediumm text-lg">
-                <span className="font-thin text-gray-400">Email:</span>{" "}
-                meskatcse@gmail.com
-              </p>
-            </td>
-            <td className="py-1.5 align-text-top">
-              <p className="font-mediumm text-lg">
-                <span className="font-thin text-gray-400">Experience:</span> 1
-                Year
-              </p>
-            </td>
-          </tr>
-          <tr>
-            <td className="py-1.5 align-text-top">
-              <p className="font-mediumm text-lg">
-                <span className="font-thin text-gray-400">Address:</span>{" "}
-                Chattogram, Bangladesh
-              </p>
-            </td>
-            <td className="py-1.5 align-text-top">
-              <p className="font-mediumm text-lg">
-                <span className="font-thin text-gray-400">Language:</span>{" "}
-                Bangla, English
-              </p>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+
+      <div className="flex flex-col md:flex-row md:space-x-8 space-y-2 md:space-y-0">
+        <ul className="space-y-2 md:space-y-4">
+          <li>
+            <span className="font-thin text-gray-400">Name:</span> Meskatul
+            Islam
+          </li>
+          <li>
+            <span className="font-thin text-gray-400">Age:</span> 24
+          </li>
+          <li>
+            <span className="font-thin text-gray-400">Email:</span>{" "}
+            meskatcse@gmail.com
+          </li>
+          <li>
+            <span className="font-thin text-gray-400">Address:</span>{" "}
+            Chattogram, Bangladesh
+          </li>
+        </ul>
+        <ul className="space-y-2 md:space-y-4">
+          <li>
+            <span className="font-thin text-gray-400">Freelance:</span>{" "}
+            Available
+          </li>
+          <li>
+            <span className="font-thin text-gray-400">Skills:</span> Backend &
+            Web Frontend
+          </li>
+          <li>
+            <span className="font-thin text-gray-400">Experience:</span> 2 Years
+          </li>
+          <li>
+            <span className="font-thin text-gray-400">Language:</span> Bangla &
+            English
+          </li>
+        </ul>
+      </div>
       <BtnDownload />
     </div>
   );
