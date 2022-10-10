f2ba293d4150d10cbd866be544bf5ac9db66ee85
iammeskat <meskatcse@gmail.com>
2022-10-10 23:41:50 +0600

    redesign logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,8 +1,48 @@
 const Logo = () => {
   return (
     <div className="flex space-x-1">
-      <h2 className="text-xl font-['Autorich_Sans']">MESKAT</h2>
-      <span className="text-xs text-yellow-500">Testing</span>
+      <svg
+        className="w-24 md:w-32 h-auto primary-colorL"
+        viewBox="0 0 377.7 65"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+      >
+        <g
+          id="svgGroup"
+          strokeLinecap="round"
+          fillRule="evenodd"
+          //   fontSize="9pt"
+          stroke="#000"
+          strokeWidth="0.25mm"
+        >
+          <path
+            d="M 52.9 0 L 34.9 41.7 L 16.9 0 L 0 0 L 0 65 L 11 65 L 11 14.1 L 29.9 57.7 L 39.9 57.7 L 58.8 14.1 L 58.8 65 L 69.8 65 L 69.8 0 L 52.9 0 Z"
+            id="0"
+          />
+          <path
+            d="M 86.8 65 L 133.9 65 L 133.9 54 L 86.8 54 L 86.8 65 Z M 86.8 0 L 86.8 11 L 133.9 11 L 133.9 0 L 86.8 0 Z M 128.4 27.5 L 86.8 27.5 L 86.8 38.5 L 128.4 38.5 L 128.4 36.3 L 128.4 27.5 Z"
+            id="1"
+          />
+          <path
+            d="M 177.8 27 L 167.9 27 C 163.5 27 159.9 23.4 159.9 19 C 159.9 14.6 163.5 11 167.9 11 L 194.6 11 L 194.6 0 L 167.9 0 C 157.4 0 148.9 8.5 148.9 19 C 148.9 29.5 157.4 38 167.9 38 L 177.8 38 C 182.2 38 185.8 41.6 185.8 46 C 185.8 50.4 182.2 54 177.8 54 L 151.1 54 L 151.1 65 L 177.8 65 C 188.3 65 196.8 56.5 196.8 46 C 196.8 35.5 188.3 27 177.8 27 Z"
+            id="2"
+          />
+          <path
+            d="M 240.2 31.6 L 261.2 0 L 248.2 0 L 223.8 36.7 L 223.8 0 L 212.8 0 L 212.8 65 L 223.8 65 L 223.8 56.3 L 234.1 40.8 C 242.6 45.8 248 55 248 65 L 259 65 C 259 51.3 251.7 38.7 240.2 31.6 Z"
+            id="3"
+          />
+          <path
+            d="M 320.4 65 L 332.4 65 L 304.2 0 L 291.9 0 L 263.7 65 L 275.7 65 L 298.1 13.5 L 320.4 65 Z"
+            id="4"
+          />
+          <path
+            d="M 377.7 11 L 377.7 0 L 324.9 0 L 324.9 11 L 377.7 11 Z M 345.8 18.4 L 345.8 65 L 356.8 65 L 356.8 18.4 L 345.8 18.4 Z"
+            id="5"
+          />
+        </g>
+      </svg>
+      {/* <h2 className="text-xl font-['Autorich_Sans']">MESKAT</h2> */}
+      <span className="text-xs text-yellow-500 -mt-1">Testing</span>
     </div>
   );
 };
