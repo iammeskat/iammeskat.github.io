22ae2e28e0af9243a238808302a46070cbb20395
iammeskat <meskatcse@gmail.com>
2022-10-13 13:21:10 +0600

    add link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,18 @@
+import { Link } from "react-scroll";
 const Footer = () => {
   return (
-    <footer className="flex flex-col items-end bg-[#1a1a1a] p-2 text-gray-800m text-slate-400 text-xs">
+    <footer className="flex flex-col items-center bg-[#1a1a1a] p-2 text-gray-800m text-slate-400 text-xs">
       <p>© Copyright {new Date().getFullYear()}</p>
       <p>
         Design & developed by{" "}
-        <span className="font-medium primary-color">Meskatul Islam</span>
+        <Link
+          activeClass="active"
+          to="home"
+          spy
+          className="font-medium primary-color cursor-pointer"
+        >
+          Meskatul Islam
+        </Link>
       </p>
     </footer>
   );
