bb94da750b98e32c6a3dcbfe06f4b68886cf372e
iammeskat <meskatcse@gmail.com>
2022-10-14 01:30:12 +0600

    update intro

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,10 +12,10 @@ const Home = () => {
           <h2 className="font-bold text-4xl md:text-6xl truncate">
             Meskatul Islam
           </h2>
-          <p className="font-medium text-xl md:text-2xl primary-color">
+          <p className="text-center lg:text-left font-medium text-xl md:text-2xl primary-color">
             Software Engineer | Programmer
           </p>
-          <p className="text-centerm w-full lg:max-w-[35rem] lg:text-left text-justify">
+          <p className="text-center w-full lg:max-w-[35rem] lg:text-left">
             Hello, I’m a full stack web developer from Bangladesh. I have
             experience in web development, design, and customization. Also, I am
             good at database designing and software requirements analysis.
