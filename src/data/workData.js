c89c55a796e11f250611bcec19642116fc1b75b2
iammeskat <meskatcse@gmail.com>
2022-10-17 12:32:40 +0600

    dummy data

--- src/data/workData.js
+++ src/data/workData.js
@@ -1,48 +1,67 @@
 export const workData = [
+    // {
+    //     title: "E-Commerce Management System",
+    //     img: "blog2.jpg",
+    //     description: "Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Commodi, Illum.",
+    //     github: "https://github.com/iammeskat/",
+    //     live: "#"
+    // },{
+    //     title: "Student Infromation Management System",
+    //     img: "blog2.jpg",
+    //     description: "Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Commodi, Illum.",
+    //     github: "https://github.com/iammeskat/",
+    //     live: "#"
+    // },{
+    //     title: "Hotel Info & Guest Monitoring System",
+    //     img: "blog2.jpg",
+    //     description: "Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Commodi, Illum.",
+    //     github: "https://github.com/iammeskat/",
+    //     live: "#"
+    // },{
+    //     title: "E-Commerce Web Template",
+    //     img: "blog2.jpg",
+    //     description: "Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Commodi, Illum.",
+    //     github: "https://github.com/iammeskat/",
+    //     live: "#"
+    // },{
+    //     title: "E-Commerce Web Admin Template",
+    //     img: "blog2.jpg",
+    //     description: "Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Commodi, Illum.",
+    //     github: "https://github.com/iammeskat/",
+    //     live: "#"
+    // },{
+    //     title: "CarMe UI Project",
+    //     img: "blog2.jpg",
+    //     description: "Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Commodi, Illum.",
+    //     github: "https://github.com/iammeskat/",
+    //     live: "#"
+    // },{
+    //     title: "My Personal Portfolio",
+    //     img: "blog2.jpg",
+    //     description: "Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Commodi, Illum.",
+    //     github: "https://github.com/iammeskat/",
+    //     live: "#"
+    // },{
+    //     title: "E-Commerce Web Template",
+    //     img: "blog2.jpg",
+    //     description: "Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Commodi, Illum.",
+    //     github: "https://github.com/iammeskat/",
+    //     live: "#"
+    // },
     {
-        title: "E-Commerce Management System",
+        title: "Project Title Goes Here",
         img: "blog2.jpg",
         description: "Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Commodi, Illum.",
         github: "https://github.com/iammeskat/",
         live: "#"
     },{
-        title: "Student Infromation Management System",
+        title: "Project Title Goes Here",
         img: "blog2.jpg",
         description: "Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Commodi, Illum.",
         github: "https://github.com/iammeskat/",
         live: "#"
     },{
-        title: "Hotel Info & Guest Monitoring System",
-        img: "blog2.jpg",
-        description: "Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Commodi, Illum.",
-        github: "https://github.com/iammeskat/",
-        live: "#"
-    },{
-        title: "E-Commerce Web Template",
-        img: "blog2.jpg",
-        description: "Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Commodi, Illum.",
-        github: "https://github.com/iammeskat/",
-        live: "#"
-    },{
-        title: "E-Commerce Web Admin Template",
-        img: "blog2.jpg",
-        description: "Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Commodi, Illum.",
-        github: "https://github.com/iammeskat/",
-        live: "#"
-    },{
-        title: "CarMe UI Project",
-        img: "blog2.jpg",
-        description: "Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Commodi, Illum.",
-        github: "https://github.com/iammeskat/",
-        live: "#"
-    },{
-        title: "My Personal Portfolio",
-        img: "blog2.jpg",
-        description: "Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Commodi, Illum.",
-        github: "https://github.com/iammeskat/",
-        live: "#"
-    },{
-        title: "E-Commerce Web Template",
+        title: "Project Title Goes Here",
         img: "blog2.jpg",
         description: "Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Commodi, Illum.",
         github: "https://github.com/iammeskat/",
