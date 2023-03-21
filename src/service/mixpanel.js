bb6567ad343e0c4d268b7fb7158db8ab56a833f5
iammeskat <meskatcse@gmail.com>
2023-03-21 21:27:16 +0600

    feat: get client ip address

--- src/service/mixpanel.js
+++ src/service/mixpanel.js
@@ -1,8 +1,18 @@
 
+import axios from "axios";
 import mixpanel from "mixpanel-browser";
 
-export const track = (event, data = {}) => {
-    mixpanel.track(event, data);
+export const track = async (event, data = {}) => {
+    let clientInfo = {};
+    // http://www.geoplugin.net/json.gp
+    await axios.get('https://api.db-ip.com/v2/free/self').then(res => {
+        clientInfo = res?.data;
+    })
+    mixpanel.track(event, {
+        ...data,
+        ipAddress: clientInfo?.ipAddress,
+        clientInfo
+    });
 }
 export const initMixpanel = () => {
     mixpanel.init('bdada0a2f52d3685770a721a7b8e1997', { debug: false })
